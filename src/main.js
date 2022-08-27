bc77e6ad5b06a1f0e3a9fb73308c45c36a63c149
Alexia Bernad <alexia.bernad.2711@gmail.com>
2022-08-27 11:54:28 +0200

    Problème de CORS

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import App from './App.vue'
-import router from './router'
-
+import App from './App.vue';
+import router from './router';
 
 const pinia = createPinia();
-createApp(App).use(router).use(pinia).mount('#app')
+createApp(App).use(router).use(pinia).mount('#app');
 
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
+    next();
+  });
