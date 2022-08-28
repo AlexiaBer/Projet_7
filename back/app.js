8ba21c83532c7d59be94ffdd79b9c565adeeae6c
Alexia Bernad <alexia.bernad.2711@gmail.com>
2022-08-28 09:54:24 +0200

    Problème des CORS réglé.

--- back/app.js
+++ back/app.js
@@ -9,11 +9,11 @@ const postRoutes = require('./routes/post');
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
+  res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
 });
 
 app.use(bodyParser.json());
